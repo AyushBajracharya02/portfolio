e41c54dcb24e8e2643d425576bbcf9079bca6a53
Ayush Bajracharya <ayushbajracharya02@gmail.com>
2025-07-27 17:47:25 +0545

    Some responsive fixes and added mobile dialog

--- src/script/main.ts
+++ src/script/main.ts
@@ -3,10 +3,12 @@ import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { ScrollSmoother } from "gsap/ScrollSmoother";
 
-// const $ = document.querySelector.bind(document);
+const $ = document.querySelector.bind(document);
 const $$ = document.querySelectorAll.bind(document);
 Element.prototype.$ = Element.prototype.querySelector;
 Element.prototype.$$ = Element.prototype.querySelectorAll;
+EventTarget.prototype.on = EventTarget.prototype.addEventListener;
+EventTarget.prototype.off = EventTarget.prototype.removeEventListener;
 
 gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
 
@@ -16,13 +18,22 @@ let smoother = ScrollSmoother.create({
     smooth: 1.5,
 });
 
-ScrollTrigger.create({
-    trigger: "#aside",
-    endTrigger: "#smooth-content",
-    start: "top top",
-    end: "bottom bottom",
-    pin: true,
-})
+const minWidth1280px = window.matchMedia("(width >= 1280px)");
+
+if (minWidth1280px.matches) {
+    ScrollTrigger.create({
+        trigger: "#aside",
+        endTrigger: "#smooth-content",
+        start: "top top",
+        end: "bottom bottom",
+        pin: true,
+    });
+}
+
+// window.on("resize", function () { 
+
+// })
+
 
 $$(".header-link").forEach((link) => {
     link.addEventListener("click", function (e) {
@@ -31,3 +42,25 @@ $$(".header-link").forEach((link) => {
         smoother.scrollTo(target, true, "top top");
     })
 })
+
+const mobileMenu = $("#mobile-menu") as HTMLDialogElement;
+
+$("#mobile-menu-btn")?.on("click", function () {
+    mobileMenu.showModal();
+});
+
+mobileMenu.on("click", function () {
+    mobileMenu.classList.add("closing");
+    setTimeout(() => {
+        mobileMenu.classList.remove("closing");
+        mobileMenu.close();
+    }, 500);
+});
+
+mobileMenu.firstElementChild?.on("click", function (e) {
+    e.stopPropagation();
+});
+
+mobileMenu.$("#mobile-menu-close-btn")?.on("click",function(){
+    mobileMenu.click();
+});
--- src/script/types/main.d.ts
+++ src/script/types/main.d.ts
@@ -5,4 +5,8 @@ declare global {
         $: typeof Element.prototype.querySelector;
         $$: typeof Element.prototype.querySelectorAll;
     };
+    interface EventTarget {
+        on: typeof EventTarget.prototype.addEventListener;
+        off: typeof EventTarget.prototype.removeEventListener;
+    };
 };
